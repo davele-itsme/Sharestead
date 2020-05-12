a9bc323fb3d0d88c49bddd0b8e870063c6ff8cc0
Luemantis <ledaodavid@gmail.com>
2020-05-12 17:47:24 +0200

    Web Services

---
+++ app/src/main/java/dk/via/sharestead/model/Developer.java
@@ -0,0 +1,12 @@
+package dk.via.sharestead.model;
+
+public class Developer {
+    private int id;
+    private String name;
+    private String imageBackground;
+    public Developer(int id, String name, String imageBackground) {
+        this.id = id;
+        this.name = name;
+        this.imageBackground = imageBackground;
+    }
+}
---
+++ app/src/main/java/dk/via/sharestead/model/Platform.java
@@ -0,0 +1,10 @@
+package dk.via.sharestead.model;
+
+public class Platform {
+    private int id;
+    private String name;
+    public Platform(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
