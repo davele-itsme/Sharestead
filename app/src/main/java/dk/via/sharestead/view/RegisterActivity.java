4d6833da2697bf5aa3f87ea40b53aab932157bab
Luemantis <ledaodavid@gmail.com>
2020-05-16 00:25:08 +0200

    Login and Register

--- app/src/main/java/dk/via/sharestead/view/RegisterActivity.java
+++ app/src/main/java/dk/via/sharestead/view/RegisterActivity.java
@@ -1,5 +1,6 @@
 package dk.via.sharestead.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,46 +9,51 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
 
 import dk.via.sharestead.R;
 
-public class AuthenticationActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
+    private EditText emailField, passwordField, repeatPasswordField;
     private FirebaseAuth mAuth;
-    private EditText emailField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.authentication_activity);
+        setContentView(R.layout.register_activity);
         mAuth = FirebaseAuth.getInstance();
 
         emailField = findViewById(R.id.emailField);
         passwordField = findViewById(R.id.passwordField);
+        repeatPasswordField = findViewById(R.id.repeatPasswordField);
     }
 
-    public void onLoginBtnClicked(View view)
-    {
+    public void onRegisterBtnClicked(View view) {
         String email = emailField.getText().toString().trim();
         String password = passwordField.getText().toString().trim();
-        if(TextUtils.isEmpty(email))
-        {
+        String repeatPassword = repeatPasswordField.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
             showAlertDialog("Email can not be empty.");
             return;
-        }
-        else if(TextUtils.isEmpty(password))
-        {
+        } else if (TextUtils.isEmpty(password)) {
             showAlertDialog("Password can not be empty.");
+            return;
+        } else if (!password.equals(repeatPassword)) {
+            showAlertDialog("Passwords do not match.");
             return;
         }
 
-        mAuth.signInWithEmailAndPassword(email, password)
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
-                        // Sign in successful
-                        startActivity(new Intent(getApplicationContext(), PreferenceActivity.class));
+                        // Sign in success, update UI with the signed-in user's information
+                        startActivity(new Intent(getApplicationContext(), AuthenticationActivity.class));
                     } else {
                         // If sign in fails, display a message to the user.
                         showAlertDialog(task.getException().getMessage());
@@ -55,13 +61,12 @@ public class AuthenticationActivity extends AppCompatActivity {
                 });
     }
 
-    public void onRegisterClicked(View view)
+    public void onLoginClicked(View view)
     {
-        startActivity(new Intent(this, RegisterActivity.class));
+        startActivity(new Intent(this, AuthenticationActivity.class));
     }
 
-    private void showAlertDialog(String message)
-    {
+    private void showAlertDialog(String message) {
         new AlertDialog.Builder(this)
                 .setTitle("Error with login")
                 .setMessage(message)
